401b4597d920994a0c5f48fbb034732ee0d3e177
Andresrock98 <1dandresmontoya@gmail.com>
2022-03-30 22:43:12 +0200

    comentario1

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import Login from './Login';
 import MovieList from './movies/MovieList';
+import ShowMovie from './movies/ShowMovie';
+import MyMovieList from './movies/MyMovieList';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div>
         <Routes>
           <Route exact path="/" element={<Login/>} />
           <Route path="/home" element={<MovieList/>} />
+          <Route path="/home/details/:id" element={<ShowMovie/>} />
+          <Route path="/home/bookmarks" element={<MyMovieList/>} />
         </Routes>
       </div>
     </Router>
